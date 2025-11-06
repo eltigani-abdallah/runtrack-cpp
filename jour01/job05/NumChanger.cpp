fedd2360e908326fcd96023eebc172d11905cea9
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2025-11-06 14:21:33 +0100

    feat: day01 job05 finished

---
+++ jour01/job05/NumChanger.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+
+    int n;
+    int m;
+
+
+
+    std::cout << "enter the first number"<<std::endl;
+    std::cin>>n;
+    std::cout << "enter the second number"<<std::endl;
+    std::cin>>m;
+
+    int newN=m;
+    int newM=n;
+
+    std::cout<<"the first number, N was: "<<n<<std::endl;
+    std::cout<<"the second number, M was: "<<m<<std::endl;
+
+    std::cout<<"the new N is: "<<newN<<std::endl;
+    std::cout<<"the new M is: "<<newM<<std::endl;
+
+    return 0;
+
+
+}
